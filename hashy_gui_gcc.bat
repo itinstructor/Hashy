62f810de761a9c60f148c036f4c5d97f0acf4dca
William Loring <32806514+itinstructor@users.noreply.github.com>
2024-11-16 09:36:58 -0700

    Update

--- hashy_gui_gcc.bat
+++ hashy_gui_gcc.bat
@@ -4,9 +4,9 @@ python -m nuitka ^
     --lto=no ^
     --mingw64 ^
     --onefile ^
-    --windows-disable-console ^
+    --windows-console-mode=disable ^
     --enable-plugin=tk-inter ^
     --windows-icon-from-ico=encryption.ico ^
     --output-filename=hashy_gui.exe ^
-    hashy_gui_2.py
+    hashy_gui.py
 pause
